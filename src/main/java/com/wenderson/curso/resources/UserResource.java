a78af7da4a4b3c6cec66d34154dc0d2964c9a31e
Wendersonfarias <wendersonpfarias@gmail.com>
2022-04-30 18:01:48 -0400

    Entidade usuario e recurso

---
+++ src/main/java/com/wenderson/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.wenderson.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wenderson.curso.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1l, "maria", "maria@gmail.com", "65992619192", "12345");
+		
+		return ResponseEntity.ok().body(u);
+				
+	}
+}
