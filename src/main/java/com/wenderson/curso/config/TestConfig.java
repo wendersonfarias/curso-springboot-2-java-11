2a7270182a9762be3b8cc14ababbd5d39b87f7f1
Wendersonfarias <wendersonpfarias@gmail.com>
2022-05-01 18:49:57 -0400

    OrderStatus enum

--- src/main/java/com/wenderson/curso/config/TestConfig.java
+++ src/main/java/com/wenderson/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.wenderson.curso.entities.Order;
 import com.wenderson.curso.entities.User;
+import com.wenderson.curso.entities.enums.OrderStatus;
 import com.wenderson.curso.repositories.OrderRepository;
 import com.wenderson.curso.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		User u3 = new User(null, "Wenderson Farias", "wenderson@gmail.com","65992619192", "123789");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
